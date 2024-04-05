5bec87ed883c9e0b488314676a8803200dd0d4f0
VL <lusakvm@gmail.com>
2024-04-05 12:46:09 +0300

    init + task from ch2

---
+++ LearningPython/palindrome.py
@@ -0,0 +1,10 @@
+# Python code​​​​​​‌​‌​‌‌‌‌​​‌‌​​‌‌‌‌‌‌​​​​​ below
+# Use print("messages...") to debug your solution.
+import re
+
+show_expected_result = False
+show_hints = False
+
+def is_palindrome(teststr):
+    teststr_lower = re.sub('[^0-9a-zA-Z]+', '', teststr.lower())
+    return teststr_lower == teststr_lower[::-1]
